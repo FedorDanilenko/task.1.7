1f701e03bb9602fe325857c0ef3fc2a06154a88d
FedorDanilenko <divine.world.mail@gmail.com>
2022-06-02 15:51:49 +0300

    implement stack class

---
+++ src/my_regex.hxx
@@ -0,0 +1,11 @@
+#ifndef my_reg_ex
+#define my_reg_ex
+
+#include <regex>
+
+namespace my_regex {
+    unsigned CountSingleLetWords(std::string& file_name);
+    unsigned CountVowels(std::string& file_name);
+}
+
+#endif
